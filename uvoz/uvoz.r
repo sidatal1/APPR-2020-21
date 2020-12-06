85d286e1df8be1ef72715ade4491e0492b38c56e
Sida Talovic <talovic.sida@gmail.com>
2020-12-06 22:17:12 +0100

    spremenjena imena stolpcev

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,29 +1,22 @@
 # 2. faza: Uvoz podatkov
 
-library("dplyr")
-library("readxl")
-library("openxlsx")
-require("readr")
-require("tidyr")
-require("tibble")
-require("httr")
-library("gsubfn")
-require("rvest")
-library("tidyverse")
-require("stringr")
-
 
 # 1.tabela
 
-uvoz_1 <- read.csv2("podatki/1.tabela1.csv",
-                   col.names = "stolpec") %>% 
-                   separate(stolpec, c("leto", "Spol", "Izobrazba","starost", "enota", "Vrednost"), '","' ) %>%
-                   separate("leto", c("Leto", "Država"), ",")  %>% 
-                   select(-starost, -enota) 
+uvoz_1 <- read_csv("podatki/1.tabela1.csv",
+                   skip = 1,
+                   col_names = "stolpec") %>% 
+                   separate(stolpec, c("Leto1", "Spol", "Izobrazba", "Starost", "Enota", "Vrednost"), '","' ) %>%
+                   separate("Leto1", c("Leto", "Drzava"), ",")  %>% 
+                   select(-Starost, -Enota) %>%
+                   mutate(Vrednost = as.character(gsub('"',"", Vrednost))) %>%
+                   mutate(Drzava = as.character(gsub('"',"", Drzava)))
+                            
 
                  
 uvoz_1$Leto <- parse_integer(uvoz_1$Leto)
-
+#as.numeric(as.factor(uvoz_1["Vrednost"]))
+#sapply(uvoz_1, class)
 
 
 
@@ -31,9 +24,9 @@ uvoz_1$Leto <- parse_integer(uvoz_1$Leto)
 # 2.tabela
  
 uvoz_2 <- read_xlsx("podatki/2.tabela.xlsx",
-                     col_names= c("Leto", "Regija", "Spol", "Brez izobrazbe", "5",
-                                  "Osnovnošolska", "7", "Nižja ali srednja poklicna",
-                                  "9", "Srednja strokovna, splošna", "11", "Višješolska, visokošolska", "13"),
+                     col_names= c("Leto", "Regija", "Spol", "Brez_izobrazbe", "5",
+                                  "Osnovnosolska", "7", "Nizja_ali_srednja_poklicna",
+                                  "9", "Srednja_strokovna, splosna", "11", "Višjesolska_ali_visokosolska", "13"),
                      na= "N",
                      skip=4,
                      n_max=48) %>%
@@ -41,12 +34,11 @@ uvoz_2 <- read_xlsx("podatki/2.tabela.xlsx",
  
 # 3.tabela
 
-uvoz_3 <- read_xlsx("podatki/3.tabela.xlsx",
-                     col_names= c("Trajanje", "Leto", "Moški-VS", "4", "Moški-ZS",
-                                  "6", "Ženske-VS", "8", "Ženske-ZS", "10"),
-                     skip=5,
-                     n_max=49) %>%
-                     select(-4,-6,-8,-10) %>% fill(1)
+uvoz_3 <- read_xlsx("podatki/3.tabela1.xlsx",
+                     col_names= c("Trajanje", "Spol", "Leto", "Vzhodna_Slovenija", "Zahodna_Slovenija"),
+                    skip = 4,
+                    na="N") %>% fill(1:2)
+                     
                      
 
 
