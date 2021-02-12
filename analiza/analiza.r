38518eb757b5394c55a5f230d33e5211969ebf9e
Sida Talovic <talovic.sida@gmail.com>
2021-02-12 18:02:53 +0100

    narejena napoved in analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,37 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+theme_set(theme_bw())
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+#napoved Turcija
+podatki <- uvoz_1 %>%
+  filter(Drzava== "Turkey") %>%
+  group_by(Leto, Spol) %>%
+  summarize(vrednost = round(mean(Vrednost), digits=2)) 
+
+model <- loess(data=podatki, vrednost ~ Leto, control = loess.control(surface = "direct"))
+leto <- data.frame(Leto=seq(2008,2030,1))
+napoved <- mutate(leto, vrednost = predict(model, leto))
+
+graf_napoved <- napoved %>%
+  ggplot(aes(x=Leto, y=vrednost)) +
+  geom_smooth(method="loess", fullrange=TRUE, color="red", formula=y ~ x) +
+  geom_point(size=1, color="blue") +
+  scale_x_continuous('Leto', breaks = seq(2008, 2030, 3)) +
+  ylab("Stopnja brezposelnosti") +
+  labs(title = "Napoved stopnje brezposelnosti \n za Turčijo do leta 2030")
+graf_napoved
+
+
+#analiza Slovenija
+
+analiza1 <- ggplot(uvoz_4, aes(x=Leto, y=Stopnja_brezposelnosti)) +
+  geom_point(size=1, col="blue") +
+  geom_smooth(method="loess", formula= y~x, color="deeppink", size=0.2) +
+  facet_wrap(~regija) +
+  scale_x_continuous("Leto",  breaks = seq(2008, 2019, 5)) +
+  theme(panel.background = element_rect((fill="white")), axis.text= element_text(size=6), 
+        text= element_text(size=10), strip.background =element_rect(fill="white"), 
+        strip.text.x = element_text(size = 4)) +
+  labs("Analiza % brezposelnosti po regijah") +
+  ylab("% brezposelnosti")
+analiza1
